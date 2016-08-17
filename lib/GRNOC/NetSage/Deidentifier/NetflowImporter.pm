f2de54e5027c03d6cc7bf307534a61e82e38e287
Michael Johnson <mj82@grnoc.iu.edu>
2016-08-17 20:59:04 +0000

    Add netflow importer

---
+++ lib/GRNOC/NetSage/Deidentifier/NetflowImporter.pm
@@ -0,0 +1,248 @@
+package GRNOC::NetSage::Deidentifier::NetflowImporter;
+
+use Moo;
+
+use GRNOC::Log;
+use GRNOC::Config;
+
+use Net::AMQP::RabbitMQ;
+use JSON::XS;
+use Math::Round qw( nlowmult nhimult );
+use List::MoreUtils qw( natatime );
+use Try::Tiny;
+
+use Data::Dumper;
+
+### constants ###
+
+use constant QUEUE_PREFETCH_COUNT => 20;
+use constant QUEUE_FETCH_TIMEOUT => 10 * 1000;
+use constant RECONNECT_TIMEOUT => 10;
+use constant RAW_FLOWS_QUEUE_CHANNEL => 1;
+
+### required attributes ###
+
+has config_file => ( is => 'ro',
+                required => 1 );
+
+has logging_file => ( is => 'ro',
+                      required => 1 );
+
+has flowpath => ( is => 'ro',
+                      required => 1 );
+
+
+### internal attributes ###
+
+has logger => ( is => 'rwp' );
+
+has config => ( is => 'rwp' );
+
+has is_running => ( is => 'rwp',
+                    default => 0 );
+
+has rabbit => ( is => 'rwp' );
+
+has json => ( is => 'rwp' );
+
+has json_data => ( is => 'rwp' );
+
+has status => ( is => 'rwp' );
+
+has min_bytes => ( is => 'rwp',
+                   default => 500000000 ); # 500 MB
+
+### constructor builder ###
+
+sub BUILD {
+
+    my ( $self ) = @_;
+
+    # create and store logger object
+    my $grnoc_log = GRNOC::Log->new( config => $self->logging_file );
+    my $logger = GRNOC::Log->get_logger();
+
+    $self->_set_logger( $logger );
+
+    # create and store config object
+    my $config = GRNOC::Config->new( config_file => $self->config_file,
+                                     force_array => 0 );
+
+    $self->_set_config( $config );
+
+    return $self;
+}
+
+### public methods ###
+
+sub run {
+
+    my ( $self ) = @_;
+
+    $self->logger->debug( "Running." );
+
+    # change our process name
+    $0 = "netsage raw netflow importer";
+
+    # create JSON object
+    my $json = JSON::XS->new();
+
+    $self->_set_json( $json );
+
+    # connect to rabbit queues
+    $self->_rabbit_connect();
+
+    # continually consume messages from rabbit queue, making sure we have to acknowledge them
+    #$self->logger->debug( 'Starting RabbitMQ consume loop.' );
+
+    my $success = $self->_get_flow_data();
+
+    if ( !$success ) {
+        $self->logger->debug('Error retrieving data');
+        return;
+    }
+    $self->logger->debug('Data retrieved; sending to rabbit');
+    return $self->_publish_data();
+
+}
+
+sub _get_flow_data {
+    my ( $self ) = @_;
+
+    my @all_data = ();
+
+    my $path = $self->flowpath;
+    my $min_bytes = $self->min_bytes;
+
+    my $command = "|/usr/bin/nfdump -R $path";
+    $command .= ' -o csv -o "fmt:%ts,%te,%td,%sa,%da,%sp,%dp,%pr,%flg,%fwd,%stos,%ipkt,%ibyt,%opkt,%obyt,%in,%out,%sas,%das,%smk,%dmk,%dtos,%dir,%nh,%nhb,%svln,%dvln,%ismc,%odmc,%idmc,%osmc,%mpls1,%mpls2,%mpls3,%mpls4,%mpls5,%mpls6,%mpls7,%mpls8,%mpls9,%mpls10,%ra,%eng,%bps,%pps,%bpp"';
+    $command .= ' bytes\>' . $min_bytes;
+    $command .= " -N -q";
+    $command .= ' > test.csv ';
+    warn "command:\n\n$command\n\n";
+    my $fh;
+    open($fh, $command);
+
+    return;
+
+    #my $files = $self->flowpath;
+
+    ## TODO: update
+    #foreach my $file ( @$files ) {
+    #    my $json_data;
+    #    {
+    #        local $/; #Enable 'slurp' mode
+    #        open my $fh, "<", $file || die ('error loading json file');
+    #        $json_data = <$fh>;
+    #        close $fh;
+    #    }
+
+    #    my $data;
+    #    try {
+    #        $data = $self->json->decode( $json_data );
+    #        push @all_data, @$data;
+    #    }
+    #    catch {
+    #        $self->logger->warn( "Unable to JSON decode message: $_" );
+    #    };
+    #}
+    $self->_set_json_data( \@all_data );
+
+    warn "all floows: " . @all_data;
+
+    if (!@all_data) {
+        return;
+    } else {
+        return 1;
+    }
+
+};
+
+### private methods ###
+
+sub _publish_data {
+    my ( $self ) = @_;
+    if ( !$self->json_data ) {
+        $self->logger->info("No data found to publish");
+        return;
+    }
+    my $data = $self->json_data;
+
+    # send a max of 100 messages at a time to rabbit
+    my $it = natatime( 100, @$data );
+
+    #my $queue = $self->config->get( '/config/rabbit/raw-queue' );
+    my $rabbit_conf = $self->config->get( '/config/rabbit' );
+    my $queue = $rabbit_conf->{'queue'}->{'raw'}->{'rabbit_name'};
+
+    while ( my @finished_messages = $it->() ) {
+
+        $self->rabbit->publish( RAW_FLOWS_QUEUE_CHANNEL, $queue, $self->json->encode( \@finished_messages ), {'exchange' => ''} );
+
+    }
+
+}
+
+
+sub _rabbit_connect {
+
+    my ( $self ) = @_;
+
+    my $rabbit_host = $self->config->get( '/config/rabbit/host' );
+    my $rabbit_port = $self->config->get( '/config/rabbit/port' );
+    my $rabbit_username = $self->config->get( '/config/rabbit/username' );
+    my $rabbit_password = $self->config->get( '/config/rabbit/password' );
+    my $rabbit_vhost = $self->config->get( '/config/rabbit/vhost' );
+    my $rabbit_ssl = $self->config->get( '/config/rabbit/ssl' ) || 0;
+    my $rabbit_ca_cert = $self->config->get( '/config/rabbit/cacert' );
+    my $rabbit_conf = $self->config->get( '/config/rabbit' );
+    my $raw_data_queue = $rabbit_conf->{'queue'}->{'raw'}->{'rabbit_name'};
+    while ( 1 ) {
+
+        $self->logger->info( "Connecting to RabbitMQ $rabbit_host:$rabbit_port." );
+
+        my $connected = 0;
+
+        try {
+
+            my $rabbit = Net::AMQP::RabbitMQ->new();
+
+            my $params = {};
+            $params->{'port'} = $rabbit_port;
+            $params->{'user'} = $rabbit_username;
+            $params->{'password'} = $rabbit_password;
+            if ( $rabbit_ssl ) {
+                $params->{'ssl'} = $rabbit_ssl;
+                $params->{'ssl_verify_host'} = 0;
+                $params->{'ssl_cacert'} = $rabbit_ca_cert;
+            }
+            if ( $rabbit_vhost ) {
+                $params->{'vhost'} = $rabbit_vhost;
+            }
+
+            $rabbit->connect( $rabbit_host, $params  );
+
+
+	    # open channel to the pending queue we'll read from
+            $rabbit->channel_open( RAW_FLOWS_QUEUE_CHANNEL );
+            $rabbit->queue_declare( RAW_FLOWS_QUEUE_CHANNEL, $raw_data_queue, {'auto_delete' => 0} );
+            $rabbit->basic_qos( RAW_FLOWS_QUEUE_CHANNEL, { prefetch_count => QUEUE_PREFETCH_COUNT } );
+
+            $self->_set_rabbit( $rabbit );
+
+            $connected = 1;
+        }
+
+        catch {
+
+            $self->logger->error( "Error connecting to RabbitMQ: $_" );
+        };
+
+        last if $connected;
+
+        $self->logger->info( "Reconnecting after " . RECONNECT_TIMEOUT . " seconds..." );
+        sleep( RECONNECT_TIMEOUT );
+    }
+}
+
+1;
