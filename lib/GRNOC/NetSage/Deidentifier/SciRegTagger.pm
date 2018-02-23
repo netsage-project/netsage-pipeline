23fb2860157cc3ee2d6c303075a6f03c57f9d3fd
Michael Johnson <mj82@globalnoc.iu.edu>
2018-02-23 03:36:59 +0000

    Get recursive processing of science registry data working

--- lib/GRNOC/NetSage/Deidentifier/SciRegTagger.pm
+++ lib/GRNOC/NetSage/Deidentifier/SciRegTagger.pm
@@ -17,6 +17,8 @@ use Text::Unidecode;
 use Digest::SHA;
 use utf8;
 use JSON::XS;
+use Time::HiRes qw( time );
+
 
 use Data::Dumper;
 
@@ -29,11 +31,17 @@ has json => ( is => 'rwp' );
 
 has scireg => ( is => 'rwp' );
 
+has start_time => ( is => 'rwp' );
+has msg_count => ( is => 'rwp' );
+
 ### constructor builder ###
 
 sub BUILD {
 
     my ( $self ) = @_;
+
+    $self->_set_start_time( time() );
+    $self->_set_msg_count( 0 );
 
     my $config = $self->config;
     warn "config " . Dumper $config;
@@ -57,20 +65,21 @@ sub BUILD {
 sub _process_messages {
     my ( $self, $messages ) = @_;
     my $scireg = $self->scireg;
+    my $msg_count = $self->msg_count;;
 
     foreach my $row (@$messages ) {
         my $src = $row->{'meta'}->{'src_ip'};
         my $dst = $row->{'meta'}->{'dst_ip'};
         my $src_meta = $scireg->get_metadata( $src );
-        $self->hash_walk( $src_meta );
+#warn "src_meta\n" . Dumper $src_meta;
 
         my $dst_meta = $scireg->get_metadata( $dst );
-        $self->hash_walk( $dst_meta );
 
         if ( $src_meta ) {
             delete $src_meta->{'addresses_str'};
             delete $src_meta->{'addresses'};
             delete $src_meta->{'ip_block_id'};
+            $self->hash_walk( $src_meta, [] );
             $row->{'meta'}->{'scireg'}->{'src'} = $src_meta;
         }
         if ( $dst_meta ) {
@@ -78,40 +87,73 @@ sub _process_messages {
             delete $dst_meta->{'addresses_str'};
             delete $dst_meta->{'addresses'};
             delete $dst_meta->{'ip_block_id'};
+            $self->hash_walk( $dst_meta, [] );
             $row->{'meta'}->{'scireg'}->{'dst'} = $dst_meta;
         }
+        $msg_count++;
+        $self->_set_msg_count( $msg_count );
 
     }
 
+    my $start = $self->start_time;
+    my $now = time();
+    my $delta = $now - $start;
+    warn "finished batch ... time elapsed: " . $delta;
+    warn "messages " . $msg_count . " per second: " . ( $msg_count  / $delta );
     return $messages;
 }
 
 # recursively walk hash and format characters as needed
 sub hash_walk {
-    my ($self, $hash) = @_;
-    while (my ($k, $v) = each %$hash) {
+    my ($self, $hash, $key_list )  = @_;
+    #warn "now walking hash" . Dumper $hash;
+    foreach my $k ( keys %$hash ) { 
+        my $v = $hash->{ $k };
+         # Keep track of the hierarchy of keys, in case
+         # our callback needs it.
+         push @$key_list, $k;
+        #warn "k: $k; v: " . Dumper $v;
+        #warn "v:\n" . Dumper $v;
+        #warn "REF " . ref($v);
 
         if (ref($v) eq 'HASH' ) {
-            # Recurse.
-            $self->hash_walk($v);
+            # Recurse. I think this never happens with current data structure?
+            #warn "CURRENT V " . Dumper $v;
+            #warn "RECURSING INTO: " . $v;
+            $self->hash_walk($v, $key_list);
         } elsif ( ref($v) eq 'ARRAY' ) {
+            #warn "V is an ARRAY " . Dumper $v;
+            my $i = 0;
             foreach my $row (@$v) {
+                #warn "ARRAY ROW: " . Dumper $row;
+                #warn "ROW REF " . ref ($row );
                 if ( ref ( $row ) eq 'HASH' ) {
-                    $self->hash_walk($row);
+                    #warn "RECURSING INTO ARRAY ROW " . Dumper $row;
+                    $self->hash_walk($row, $key_list);
+                } elsif ( ref ($row) eq '' ) {
+                    #warn "no ref row " . Dumper $row;
+                    $row = _convert_chars($row);
+                    $v->[$i] = $row;
+
                 }
+                $i++;
             }
         } else {
+            #warn "converting characters - ref: '" . ref($v) . "' , " . $v;
             # Otherwise, convert the characters
             my $newv = _convert_chars( $v );
             $hash->{ $k } = $newv;
         }
-
+        pop @$key_list;
     }
 }
 
 # formatting callback
 sub _convert_chars {
     my ( $input ) = @_;
+    if ( !defined ( $input ) ) {
+        return undef;
+    }
     $input = unidecode( $input );
     return $input;
 }
