1652c9a64221f760e8b361c07028a2c5a998ab84
Samir <samir@es.net>
2020-05-18 12:12:22 -0700

    Fixing behavior to avoid pipeline interruptions.

--- compose/pipeline/docker_init.sh
+++ compose/pipeline/docker_init.sh
@@ -2,6 +2,16 @@
 
 mkdir -p /data/cache/ && echo "Cache directory created" || echo "cache dir already exists"
 
-wget  https://scienceregistry.grnoc.iu.edu/exported/GeoLite2-ASN.mmdb --no-use-server-timestamps -q -O /data/cache/GeoLite2-ASN.mmdb
-wget  https://scienceregistry.grnoc.iu.edu/exported/scireg.mmdb --no-use-server-timestamps -q -O /data/cache/scireg.mmdb
-wget  https://scienceregistry.grnoc.iu.edu/exported/GeoLite2-City.mmdb --no-use-server-timestamps -q -O /data/cache/GeoLite2-City.mmdb
+FILES="GeoLite2-ASN scireg GeoLite2-City"
+
+## Download all files to temporary destination
+for f in $FILES 
+do
+    wget  https://scienceregistry.grnoc.iu.edu/exported/$f.mmdb --no-use-server-timestamps -q -O /data/cache/$f.tmp
+done
+
+## Rename the temporary files to replace the production ones.
+for f in $FILES 
+do
+    mv /data/cache/$f.tmp /data/cache/$f.mmdb
+done
