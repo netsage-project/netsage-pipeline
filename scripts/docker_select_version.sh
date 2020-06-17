50f777c9ed1c963f0b9a6dd0331b131177664946
Samir <samir@es.net>
2020-06-17 11:39:42 -0700

    Doing Tag Based Release

---
+++ scripts/docker_select_version.sh
@@ -0,0 +1,27 @@
+#!/usr//bin/env bash
+
+function displayVersions() {
+  echo "Current Released Tags"
+  git tag -l
+}
+
+function selectVersion() {
+  displayVersions
+  echo -n "Which version would you like to use?  "
+  read version
+  updateDocker $version
+
+}
+
+function updateDocker() {
+  version=$1
+  echo "Selecting $version, updating docker configuration"
+  sed -i -e "s/:latest/:$version/" docker-compose.build.yml
+  sed -i -e "s/:latest/:$version/" docker-compose.yml
+}
+
+if [ $# -gt 0 ]; then
+  updateDocker $1
+else
+  selectVersion
+fi
--- scripts/integration.sh
+++ scripts/integration.sh
@@ -1,38 +1,45 @@
-#!/usr/bin/env bash 
+#!/usr/bin/env bash
 set -e
 
 ## Validates code and publish image for tagged branch
-function integration_test {
+function integration_test() {
+    echo "Tag value is set to ${TRAVIS_TAG}"
+
+    if [[ -z ${TRAVIS_TAG} ]]; then
+        echo "Tag is not set, skipping"
+    else
+        echo "Updating configuration to select tag"
+        ./scripts/docker_select_version.sh ${TRAVIS_TAG}
+
+    fi
+
     docker-compose -f docker-compose.build.yml build
-    
-    if [ "$TRAVIS_PULL_REQUEST" = "false" ]; then 
+
+    if [ "$TRAVIS_PULL_REQUEST" = "false" ]; then
         publish_image
     fi
 }
 
-
 ## Publish image to our docker hub repository
-function publish_image
-{
+function publish_image() {
     echo "$DOCKER_PASSWORD" | docker login -u "$DOCKER_USERNAME" --password-stdin
     docker-compose -f docker-compose.build.yml push
 }
 
-function sanity_test
-{
+function sanity_test() {
     echo "Executing Sanity Check Test"
     #Create env file
     cp env.example .env
     # Bringing up the container
-    docker-compose up -d 
+    docker-compose up -d
     # Waiting for something to go wrong
-    sleep 60 
+    sleep 60
     ## Get count of contains still up and running
     cnt=$(docker-compose ps | grep "Up" | wc -l)
     ## Validate count is 5, otherwise we fail
-    if [ "$cnt" -ne 5 ]; then 
-        failed_containers=$(docker-compose ps | grep -v "Up"  | grep  "pipeline" | awk '{print $1}' | cut -d '_' -f 2)
-        docker-compose logs $failed_containers        
+    if [ "$cnt" -ne 5 ]; then
+        failed_containers=$(docker-compose ps | grep -v "Up" | grep "pipeline" | awk '{print $1}' | cut -d '_' -f 2)
+        docker-compose logs $failed_containers
         echo "Sanity check failed expected 5 containers running and got $cnt"
         exit 1
     fi
@@ -41,25 +48,22 @@ function sanity_test
 
 }
 
-
 # Sets up docker compose for regression testing
-function setupDocker 
-{
+function setupDocker() {
     sudo rm /usr/local/bin/docker-compose
-    curl -L https://github.com/docker/compose/releases/download/${DOCKER_COMPOSE_VERSION}/docker-compose-`uname -s`-`uname -m` > docker-compose
+    curl -L https://github.com/docker/compose/releases/download/${DOCKER_COMPOSE_VERSION}/docker-compose-$(uname -s)-$(uname -m) >docker-compose
     chmod +x docker-compose
     sudo mv docker-compose /usr/local/bin
 }
 
 ## Brings up the dashboard and executes the regression tests
-function cron_regression {
+function cron_regression() {
     echo "Cron integration not supported"
 }
 
 # Entry point
-function main
-{
-    if [[ "$TRAVIS_EVENT_TYPE" = "cron" ]]; then 
+function main() {
+    if [[ "$TRAVIS_EVENT_TYPE" = "cron" ]]; then
         cron_regression
     else
         integration_test
