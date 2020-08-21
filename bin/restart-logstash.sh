35376ccd3b3a40d163da2e0460e0a453e2758333
Lisa Ensman <lensman@globalnoc.iu.edu>
2020-08-21 15:43:47 +0000

    revised cron files and logstash restart script.

--- bin/restart-logstash.sh
+++ bin/restart-logstash.sh
@@ -4,14 +4,17 @@
 # This is a simple way to make sure logstash has stopped before we try to start it
 
 /sbin/service logstash stop
-sleep 7                                                    
+sleep 7
 /sbin/service logstash stop
-sleep 3
+sleep 7
 
-logstash_status=$(/sbin/service logstash status)  
+logstash_status=$(/sbin/service logstash status)
 if [[ $logstash_status =~ .*"active (running)".*|"logstash is running" ]]; then
     echo "Logstash has not stopped. Cannot restart."
 else
-    echo "Logstash has stopped. Attempting to restart."
+    echo "Logstash has stopped."
+    echo "ls /tmp/:"
+    ls /tmp
+    echo "Attempting to start logstash."
     /sbin/service logstash start
 fi
