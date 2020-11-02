07142b51ff908140f7257822c9cba001c6f04fbf
Lisa Ensman <lensman@globalnoc.iu.edu>
2020-11-02 19:30:55 +0000

    removed ref to ampath; changed ref to scireg .netsage.global; updated restart-logstash.sh

--- bin/restart-logstash.sh
+++ bin/restart-logstash.sh
@@ -1,21 +1,30 @@
 #!/bin/bash
+
 # Logstash restart script
 # Sometimes logstash is slow to stop. Trying twice normally works.
 # This is a simple way to make sure logstash has stopped before we try to start it
 
-echo "date"
+echo "$(date)"
+echo "stopping logstash"
 /sbin/service logstash stop
-sleep 7
+sleep 30
+    echo "(after 30s) ls /tmp/:"
+    ls /tmp
+
+echo "$(date)"
+echo "stopping logstash"
 /sbin/service logstash stop
-sleep 7
+sleep 30
+    echo "(after 30s) ls /tmp/:"
+    ls /tmp
 
 logstash_status=$(/sbin/service logstash status)
 if [[ $logstash_status =~ .*"active (running)".*|"logstash is running" ]]; then
+    #echo $logstash_status
     echo "Logstash has not stopped. Cannot restart."
 else
+    echo "$(date)"
     echo "Logstash has stopped."
-    echo "ls /tmp/:"
-    ls /tmp
     echo "Attempting to start logstash."
     /sbin/service logstash start
 fi
