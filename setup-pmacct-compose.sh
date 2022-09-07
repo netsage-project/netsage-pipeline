b473eb8896b52aadee53bc76c5ac7e03acd0e2dc
Lisa Ensman <lensman@globalnoc.iu.edu>
2022-09-07 18:20:43 +0000

    Changed to having script create docker-compose.yml based on example and env files. And added blocks of resusable default settings to compose file.

--- setup-pmacct-compose.sh
+++ setup-pmacct-compose.sh
@@ -1,8 +1,10 @@
 #!/bin/bash
 
 # This script reads pmacct env variables from the .env file,
-# creates config files from the examples, and copies the env variable
+# [re]creates pmacct config files from the examples, and copies the env variable
 # values into them. (Needed because pmacct doesn't support using env vars)
+# It also [re]creates the docker-compose.yml file based on .env file entries. 
+
 echo ""
 
 # Get env variables from .env file
@@ -30,13 +32,17 @@ do
     fi
 done < "$input"
 
-# Loop over sflow sensors / create config files
+# Create the docker-compose.yml file by copying the example (will overwrite any existing)
+echo "Creating docker-compose.yml."
+# Delete all the pmacct services, ie, everything between "services:" and "rabbit:"
+# -0777 = treat the whole file as one string; -e code-to-run; .../s = interpret . as any char or newline.
+perl -0777 -pe "s/services:.*rabbit:/services:\n\nINSERT-HERE\n\n  rabbit:/s" < docker-compose.example.yml > docker-compose.yml
+
+# Loop over sflow sensors / create config files (will overwrite any existing)
 port=8000
 for (( n=1; n<=${sflowSensors}; n++ ))
 do
     # assign the port the container will use
-    # (Note that it is important to have the same internal (container) port numbers used for the same services (eg, _1) 
-    # every time this script is run, since an override file with hardcoded port numbers may already exist.)
     export sflowContainerPort_$n=$port
     # create temp config files
     cp conf-pmacct/sfacctd.conf.ORIG conf-pmacct/sfacctd_$n.conf.temp
@@ -49,17 +55,37 @@ do
     envsubst < conf-pmacct/sfacctd-pretag_$n.map.temp > conf-pmacct/sfacctd-pretag_$n.map
     # remove temp files
     rm conf-pmacct/*.temp
+
+    # service info for compose file; export so perl can see it.
+    export section='  sfacctd_1:
+    container_name: sfacctd_1
+    << : *pmacct-defaults
+    << : *sflow-defaults
+    command:
+      # parameters for the sfacctd command
+      - -f
+      - /etc/pmacct/sfacctd_1.conf
+    ports:
+      # port on host receiving flow data : port in the container
+      - "${sflowPort_1}:${sflowContainerPort_1}/udp"
+
+INSERT-HERE'
+
+    # substitute _$n for _1 in $section
+    section=$(sed 's/_1/_'"$n"'/g' <<< "$section")
+
+    # write it into the compose file
+    perl -i -pe 's/INSERT-HERE/$ENV{section}/' docker-compose.yml
+
     # next port number is 1 more
     port=$(($port+1))
 done
 
-# Loop over netflow sensors / create config files
+# Loop over netflow sensors / create config files (will overwrite any existing)
 port=9000
 for (( n=1; n<=${netflowSensors}; n++ ))
 do
     # assign the port the container will use
-    # (Note that it is important to have the same internal (container) port numbers used for the same services (eg, _1) 
-    # every time this script is run, since an override file with hardcoded port numbers may already exist.)
     export netflowContainerPort_$n=$port
     # create temp config files
     cp conf-pmacct/nfacctd.conf.ORIG conf-pmacct/nfacctd_$n.conf.temp
@@ -72,41 +98,42 @@ do
     envsubst < conf-pmacct/nfacctd-pretag_$n.map.temp > conf-pmacct/nfacctd-pretag_$n.map
     # remove temp files
     rm conf-pmacct/*.temp
+
+    # service info for compose file; export so perl can see it.
+    export section='  nfacctd_1:
+    container_name: nfacctd_1
+    << : *pmacct-defaults
+    << : *netflow-defaults
+    command:
+      # parameters for the nfacctd command
+      - -f
+      - /etc/pmacct/nfacctd_1.conf
+    ports:
+      # port on host receiving flow data : port in the container
+      - "${netflowPort_1}:${netflowContainerPort_1}/udp"
+
+INSERT-HERE'
+
+    # substitute _$n for _1 in $section
+    section=$(sed 's/_1/_'"$n"'/g' <<< "$section")
+
+    # write it into the compose file
+    perl -i -pe 's/INSERT-HERE/$ENV{section}/' docker-compose.yml
+
     # next port number is 1 more
     port=$(($port+1))
 done
 
-# If the docker-compose.override file doesn't exist, make it by copying the example
-if [[ ! -f "docker-compose.override.yml" ]]
-then
-    echo "Creating docker-compose.override.yml."
-    cp docker-compose.override_example.yml docker-compose.override.yml 
-fi
+# Get rid of any remaining "INSERT-HERE" lines
+    perl -i -pe 's/INSERT-HERE//' docker-compose.yml
 
-# If there are no sflow sensors, and we didn't already do it, override the sfacctd command so the container 
-# just echos a line and exits right away; and set the port env vars to defaults so docker-compose doesn't complain that either is unset
-if [[ ${sflowSensors} -eq 0 ]] &&  ! grep -ql "No Sflow collector" "docker-compose.override.yml" 
-then
-    echo "Replacing entry_point for sflow collector since it is not needed."
-    sed -i "s/sfacctd_1:/sfacctd_1:\n    entrypoint: echo 'No Sflow collector.'/" docker-compose.override.yml
-    export sflowPort_1=8000
-    export sflowContainerPort_1=8000
-fi
-# Same if no netflow sensors
-if [[ ${netflowSensors} -eq 0 ]] &&  ! grep -ql "No Netflow collector" "docker-compose.override.yml" 
-then
-    echo "Replacing entry_point for netflow collector since it is not needed."
-    sed -i "s/nfacctd_1:/nfacctd_1:\n    entrypoint: echo 'No Netflow collector.'/" docker-compose.override.yml
-    export netflowPort_1=9000
-    export netflowContainerPort_1=9000
-fi
 
-# Replace any env variables in the override file.
-envsubst < docker-compose.override.yml > docker-compose.override.yml.temp
-mv docker-compose.override.yml.temp  docker-compose.override.yml
+# Replace any env variables in the compose file.
+envsubst < docker-compose.yml > docker-compose.yml.temp
+mv docker-compose.yml.temp  docker-compose.yml
 
 echo "    Pmacct config files have been created, based on the .env file."
-echo "    Please check the docker-compose.override.yml file to be sure it matches the .env file!"
+echo "    Docker-compose.yml has been created. Please check to be sure it matches the .env file!"
 echo ""
 
 
