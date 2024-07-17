bfa3440050301d1d4d50275ac6f700742fce2b60
dojosout <dojosout@netsage-testing.tacc.utexas.edu>
2024-07-17 12:07:48 -0500

    Project cleanup of unused directories and files.

--- compose/importer/run.sh
+++
@@ -1,5 +0,0 @@
-#!/usr/bin/env bash
-
-/tmp/docker_init.sh
-
-/tmp/netsage-netflow-importer-daemon.pl --nofork  --config /tmp/conf/netsage_netflow_importer.xml --sharedconfig /tmp/conf/netsage_shared.xml
