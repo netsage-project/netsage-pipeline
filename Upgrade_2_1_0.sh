ed6821ba86cc3a755162b14e036c30b3993bdef4
Doug Southworth <46789947+dojosout@users.noreply.github.com>
2024-08-07 12:08:15 -0400

    Update Upgrade_2_1_0.sh
    Update importer from 2.1.0 to 2.1.1

--- Upgrade_2_1_0.sh
+++ Upgrade_2_1_0.sh
@@ -29,7 +29,7 @@ fi
 
 # Update docker-compose.override.yml
 sed -i -e "s|pipeline_logstash:v2.0.0|netsage_pipeline:v2.1.0|" docker-compose.override.yml
-sed -i -e "s|pipeline_importer:v2.0.0|netsage_importer:v2.1.0|" docker-compose.override.yml
+sed -i -e "s|pipeline_importer:v2.0.0|netsage_importer:v2.1.1|" docker-compose.override.yml
 sed -i -e "s|netsage-nfdump-collector:alpine-1.6.23|netsage_collector:v2.1.0|" docker-compose.override.yml
 sed -i -e "s|nfdump-collector:alpine-1.6.23|netsage_collector:v2.1.0|" docker-compose.override.yml
 sed -i -e "s|nfcapd -T all -l /data -S 1 -w -z -p|nfcapd -w /data -S 1 -z -p|" docker-compose.override.yml
@@ -37,7 +37,7 @@ sed -i -e "s|sfcapd -T all -l /data -S 1 -w -z -p|sfcapd -w /data -S 1 -z -p|" d
 
 # Check changes in docker-compose.override.yml
 check_change docker-compose.override.yml "netsage_pipeline:v2.1.0" "Failed to update pipeline_logstash to netsage_pipeline:v2.1.0"
-check_change docker-compose.override.yml "netsage_importer:v2.1.0" "Failed to update pipeline_importer to netsage_importer:v2.1.0"
+check_change docker-compose.override.yml "netsage_importer:v2.1.1" "Failed to update pipeline_importer to netsage_importer:v2.1.1"
 check_change docker-compose.override.yml "netsage_collector:v2.1.0" "Failed to update netsage-nfdump-collector to netsage_collector:v2.1.0"
 check_change docker-compose.override.yml "netsage_collector:v2.1.0" "Failed to update nfdump-collector to netsage_collector:v2.1.0"
 check_change docker-compose.override.yml "nfcapd -w /data -S 1 -z -p" "Failed to update nfcapd command"
