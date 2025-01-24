c67e7c3ee6a5486aca1869aa857a72a3657fe0cc
Brian Tierney <bltierney@gmail.com>
2025-01-24 09:40:00 -0600

    updated to work with communties.json too

--- scireg-utils/scireg2mmdb.go
+++ scireg-utils/scireg2mmdb.go
@@ -30,124 +30,106 @@ import (
 )
 
 type AddressBlock struct {
-	Addresses    []string `json:"addresses"`
-	OrgName      string   `json:"org_name"`
-	Discipline   string   `json:"discipline"`
-	Community    string   `json:"community"`
-	Latitude     string   `json:"latitude"`
-	Longitude    string   `json:"longitude"`
-	ResourceName string   `json:"resource_name"`
-        ProjectName  string   `json:"project_name"`
+    Addresses    []string `json:"addresses"`
+    OrgName      string   `json:"org_name"`
+    Discipline   string   `json:"discipline"`
+    Community    string   `json:"community"`
+    Latitude     string   `json:"latitude"`
+    Longitude    string   `json:"longitude"`
+    ResourceName string   `json:"resource_name"`
+    ProjectName  string   `json:"project_name"`
 }
 
 func main() {
-    // Define command-line flags for input and output files
-       inputFile := flag.String("i", "", "Input JSON file")
-       outputFile := flag.String("o", "", "Output MMDB file")
-       flag.Parse()
+    inputFile := flag.String("i", "", "Input JSON file")
+    outputFile := flag.String("o", "", "Output MMDB file")
+    flag.Parse()
 
-    // Check if input and output files are provided
-       if *inputFile == "" || *outputFile == "" {
-           flag.Usage()
-           os.Exit(1)
-       }
+    if *inputFile == "" || *outputFile == "" {
+        flag.Usage()
+        os.Exit(1)
+    }
 
-	// Open and read the input JSON file
-	file, err := os.Open(*inputFile)
-	if err != nil {
-		log.Fatalf("Failed to open input file: %v", err)
-	}
-	defer file.Close()
+    file, err := os.Open(*inputFile)
+    if err != nil {
+        log.Fatalf("Failed to open input file: %v", err)
+    }
+    defer file.Close()
 
-	// Parse JSON data
-	var entries []AddressBlock
-	decoder := json.NewDecoder(file)
-	if err := decoder.Decode(&entries); err != nil {
-		log.Fatalf("Failed to decode JSON: %v", err)
-	}
+    var entries []AddressBlock
+    decoder := json.NewDecoder(file)
+    if err := decoder.Decode(&entries); err != nil {
+        log.Fatalf("Failed to decode JSON: %v", err)
+    }
 
-	// Create an MMDB writer
-	writer, err := mmdbwriter.New(mmdbwriter.Options{
-            DatabaseType: "GeoLite2-City",
-            Description: map[string]string{ "en": "Fake GeoIP2-City db for Science Registry"},
-            RecordSize:   24,
-	})
-	if err != nil {
-		log.Fatalf("Failed to create MMDB writer: %v", err)
-	}
-	recordCount := 0
+    writer, err := mmdbwriter.New(mmdbwriter.Options{
+        DatabaseType: "GeoLite2-City",
+        Description: map[string]string{"en": "Fake GeoIP2-City db for Science Registry"},
+        RecordSize:   24,
+    })
+    if err != nil {
+        log.Fatalf("Failed to create MMDB writer: %v", err)
+    }
 
-	// Process each entry and add to MMDB
-	for _, entry := range entries {
+    recordCount := 0
 
-                // Convert Latitude and Longitude strings to float64
-                lat, err := strconv.ParseFloat(strings.TrimSpace(entry.Latitude), 64)
-                if err != nil {
-                     log.Printf("Warning: Failed to parse Latitude: %v", err)
-                     lat = 0.0 // Default value or handle accordingly
-                }
+    for _, entry := range entries {
+        // Print JSON content for reference
+        entryJson, _ := json.Marshal(entry)
 
-                long, err := strconv.ParseFloat(strings.TrimSpace(entry.Longitude), 64)
-                if err != nil {
-                     log.Printf("Warning: Failed to parse Latitude: %v", err)
-                     long = 0.0 // Default value or handle accordingly
-                }
+        lat, err := strconv.ParseFloat(strings.TrimSpace(entry.Latitude), 64)
+        if err != nil {
+            // log.Printf("No Latitude for entry: %s, setting to 0. ", entryJson)
+            lat = 0.0
+        }
 
-		for _, addr := range entry.Addresses {
-			_, network, err := net.ParseCIDR(addr)
-			if err != nil {
-				log.Printf("Invalid CIDR %s: %v", addr, err)
-				continue
-			}
+        long, err := strconv.ParseFloat(strings.TrimSpace(entry.Longitude), 64)
+        if err != nil {
+            // log.Printf("No Longitude for entry: %s, setting to 0. ", entryJson)
+            long = 0.0
+        }
 
-                        // Initialize jsonString
-                        var jsonString string
+        for _, addr := range entry.Addresses {
+            _, network, err := net.ParseCIDR(addr)
+            if err != nil {
+                log.Printf("Invalid CIDR %s for entry: %s, skipping. Error: %v", addr, entryJson, err)
+                continue
+            }
 
-                        // all of these fields get embedded to 'city'
-                        jsonString = fmt.Sprintf(`{"discipline": "%s", "org_name": "%s", "resource": "%s", "project": "%s", "community": "%s"}`,
-                                entry.Discipline, entry.OrgName, entry.ResourceName, entry.ProjectName, entry.Community)
+            jsonString := fmt.Sprintf(`{"discipline": "%s", "org_name": "%s", "resource": "%s", "project": "%s", "community": "%s"}`,
+                entry.Discipline, entry.OrgName, entry.ResourceName, entry.ProjectName, entry.Community)
 
-                        geoData := mmdbtype.Map{
-                                "city": mmdbtype.Map{
-                                        "names": mmdbtype.Map{
-                                                "en": mmdbtype.String(jsonString),
-                                        },
-                                },
-                                "location": mmdbtype.Map{
-                                            "latitude":  mmdbtype.Float64(lat),
-                                            "longitude": mmdbtype.Float64(long),
-                                },
-                        }
+            geoData := mmdbtype.Map{
+                "city": mmdbtype.Map{
+                    "names": mmdbtype.Map{
+                        "en": mmdbtype.String(jsonString),
+                    },
+                },
+                "location": mmdbtype.Map{
+                    "latitude":  mmdbtype.Float64(lat),
+                    "longitude": mmdbtype.Float64(long),
+                },
+            }
 
-                        // Debug: Print the record data going into the MMDB
-			//fmt.Printf("Inserting record for %s:\n", addr)
-			//for k, v := range geoData {
-		        //	fmt.Printf("  %s: %v\n", k, v)
-			//}
+            if err = writer.Insert(network, geoData); err != nil {
+                log.Printf("Error inserting record for network %s: %v\nEntry Data: %s", addr, err, entryJson)
+                continue
+            }
+            recordCount++
+        }
+    }
 
-			err = writer.Insert(network, geoData)
-			if err != nil {
-				log.Fatalf("Failed to insert record: %v", err)
-			}
-			recordCount++
-		}
+    out, err := os.Create(*outputFile)
+    if err != nil {
+        log.Fatalf("Failed to create output file: %v", err)
+    }
+    defer out.Close()
 
-	}
+    _, err = writer.WriteTo(out)
+    if err != nil {
+        log.Fatalf("Failed to write MMDB: %v", err)
+    }
 
-	// Write the MMDB file
-	out, err := os.Create(*outputFile)
-	if err != nil {
-		log.Fatalf("Failed to create output file: %v", err)
-	}
-	defer out.Close()
-
-	_, err = writer.WriteTo(out)
-	if err != nil {
-		log.Fatalf("Failed to write MMDB: %v", err)
-	}
-
-	fmt.Printf("MMDB created successfully with %d records!\n", recordCount)
-
+    fmt.Printf("MMDB created successfully with %d records!\n", recordCount)
 }
-
 
