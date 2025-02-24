436c3bb52bb45f56849359580911ee70e1c9f41d
Brian Tierney <bltierney@utexas.edu>
2025-02-24 08:16:37 -0600

    bug fixes

--- scripts/scireg_restart.sh
+++ scripts/scireg_restart.sh
@@ -17,7 +17,8 @@ initial_size=$(calculate_size)
 
 while true; do
   #check if any new files ready to download, and update if necessary
-  ./mmdb_update.sh $WATCH_DIR
+  echo "Calling scripts/mmdb_update.sh $WATCH_DIR"
+  scripts/mmdb_update.sh $WATCH_DIR
 
   current_size=$(calculate_size)
 
